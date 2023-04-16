f21ab3c62b9bf66bce3a4b40aab5fec0030efe0c
MHF2145 <117990315+MHF2145@users.noreply.github.com>
2023-04-16 11:26:33 +0700

    Update!
    Second Update

--- Pratice/Source Code/Hanif Furqan/Second/FS.cpp
+++ Pratice/Source Code/Hanif Furqan/Second/FS.cpp
@@ -1,56 +1,162 @@
 #include <iostream>
-#include <vector>
-#include <map>
+#include <queue>
 using namespace std;
 
-const int MAXN = 1003;
+typedef struct bstnode
+{
+    int data;
+    bstnode *left, *right;
+} Node;
 
-vector<int> adj[MAXN];
-int parent[MAXN], level[MAXN];
+typedef struct tree
+{
+    Node *root;
+    unsigned int size;
+} bst;
 
-void dfs(int node, int p, int lvl) {
-    parent[node] = p;
-    level[node] = lvl;
-    for (int i = 0; i < adj[node].size(); i++) {
-        if (adj[node][i] != p) {
-            dfs(adj[node][i], node, lvl + 1);
+bst pohon;
+
+void init(bst *bst)
+{
+    bst->root = NULL;
+    bst->size = 0u;
+}
+
+Node *createNode(int data)
+{
+    Node *newnode = new Node;
+    newnode->data = data;
+    newnode->left = newnode->right = NULL;
+    return newnode;
+}
+
+Node *insert(Node *root, int data)
+{
+    if (root == NULL)
+    {
+        return createNode(data);
+    }
+    if (data < root->data)
+    {
+        root->left = insert(root->left, data);
+    }
+    else if (data > root->data)
+    {
+        root->right = insert(root->right, data);
+    }
+    return root;
+}
+
+Node *search(Node *root, int data)
+{
+    while (root)
+    {
+        if (data < root->data)
+        {
+            root = root->left;
+        }
+        else if (data > root->data)
+        {
+            root = root->right;
+        }
+        else
+            return root;
+    }
+    return root;
+}
+
+bool bst_find(bst *bst, int data)
+{
+    return search(bst->root, data) != NULL;
+}
+
+void InsertTree(bst *tree, int data)
+{
+    if (bst_find(tree, data) == false)
+    {
+        tree->root = insert(tree->root, data);
+        tree->size++;
+    }
+}
+
+Node *findLCA(Node *root, int n1, int n2)
+{
+    if (root == NULL)
+        return NULL;
+
+    if (root->data > n1 && root->data > n2)
+        return findLCA(root->left, n1, n2);
+
+    if (root->data < n1 && root->data < n2)
+        return findLCA(root->right, n1, n2);
+
+    return root;
+}
+
+int CariLevel(Node *root, int data, int &level)
+{
+    if (root)
+    {
+        if (root->data == data)
+        {
+            return level;
+        }
+
+        else if (root->data < data)
+        {
+            level++;
+            return CariLevel(root->right, data, level);
+        }
+        else if (root->data > data)
+        {
+            level++;
+            return CariLevel(root->left, data, level);
         }
     }
 }
 
-int main() {
-    int n, q;
+int main()
+{
+    queue<int> Ancestor;
+    init(&pohon);
+    int n, data;
     cin >> n;
-    map<int, int> nodeMap;
-    for (int i = 1; i <= n; i++) {
-        int val;
-        cin >> val;
-        nodeMap[val] = i;
+    for (int i = 0; i < n; i++)
+    {
+
+        cin >> data;
+        InsertTree(&pohon, data);
     }
-    for (int i = 1; i < n; i++) {
-        int u, v;
-        cin >> u >> v;
-        adj[nodeMap[u]].push_back(nodeMap[v]);
-        adj[nodeMap[v]].push_back(nodeMap[u]);
-    }
-    dfs(1, 0, 1);
-    cin >> q;
-    while (q--) {
-        int k;
-        cin >> k;
-        int root = -1;
-        for (int i = 0; i < k; i++) {
-            int val;
-            cin >> val;
-            int node = nodeMap[val];
-            while (node != 0 && root == -1) {
-                if (nodeMap.count(parent[node]) == 0) {
-                    root = node;
-                }
-                node = parent[node];
-            }
+    int query;
+    cin >> query;
+    Node *temp;
+    for (int i = 0; i < query; i++)
+    {
+        int a, b;
+        cin >> a;
+        int level = 0;
+        while (a--)
+        {
+            cin >> b;
+            Ancestor.push(b);
         }
-        cout << nodeMap[root] << " " << level[root] << endl;
+        while (!Ancestor.empty() && Ancestor.size() != 1)
+        {
+            int front, next;
+            front = Ancestor.front();
+            Ancestor.pop();
+            next = Ancestor.front();
+            Ancestor.pop();
+            temp = findLCA(pohon.root, front, next);
+            Ancestor.push(temp->data);
+        }
+        if (Ancestor.size() == 1)
+        {
+            CariLevel(pohon.root, Ancestor.front(), level);
+            cout << Ancestor.front() << " " << level << endl;
+            Ancestor.pop();
+        }
     }
+    cout << endl;
     return 0;
 }
--- Pratice/Source Code/Hanif Furqan/Second/NKTCHI.cpp
+++ Pratice/Source Code/Hanif Furqan/Second/NKTCHI.cpp
@@ -1,55 +1,196 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-#include <cstring>
-
+#include <algorithm>
 using namespace std;
 
-const int MAXN = 100005;
+typedef struct bstnode
+{
+    int data, jarak;
+    bstnode *left, *right, *parent;
+} Node;
 
-int n, r, t;
-int dist[MAXN], parent[MAXN];
-vector<pair<int, int>> adj[MAXN];
+typedef struct tree
+{
+    Node *root;
+    unsigned int size;
+} bst;
 
-void bfs(int s) {
-    memset(dist, -1, sizeof(dist));
-    memset(parent, -1, sizeof(parent));
-    queue<int> q;
-    dist[s] = 0;
-    q.push(s);
-    while (!q.empty()) {
-        int u = q.front();
-        q.pop();
-        for (auto& v : adj[u]) {
-            if (dist[v.first] == -1) {
-                dist[v.first] = dist[u] + v.second;
-                parent[v.first] = u;
-                q.push(v.first);
-            }
+bst pohon;
+
+void init(bst *bst)
+{
+    bst->root = NULL;
+    bst->size = 0u;
+}
+
+Node *createNode(Node *parent, int data, int jarak)
+{
+    Node *newnode = new Node;
+    newnode->data = data;
+    newnode->jarak = jarak;
+    newnode->parent = parent;
+    newnode->left = newnode->right = NULL;
+    return newnode;
+}
+
+Node *insert(Node *root, Node *parent, int data, int jarak)
+{
+    if (root == NULL)
+    {
+        return createNode(parent, data, jarak);
+    }
+    if (data < root->data)
+    {
+        root->left = insert(root->left, root, data, jarak);
+    }
+    else if (data > root->data)
+    {
+        root->right = insert(root->right, root, data, jarak);
+    }
+    return root;
+}
+
+Node *search(Node *root, int data)
+{
+    while (root)
+    {
+        if (data < root->data)
+        {
+            root = root->left;
         }
+        else if (data > root->data)
+        {
+            root = root->right;
+        }
+        else
+            return root;
+    }
+    return root;
+}
+
+bool bst_find(bst *bst, int data)
+{
+    return search(bst->root, data) != NULL;
+}
+
+void InsertTree(bst *tree, int data, int jarak)
+{
+    if (bst_find(tree, data) == false)
+    {
+        tree->root = insert(tree->root, NULL, data, jarak);
+        tree->size++;
     }
 }
 
-int main() {
-    cin >> n >> r;
-    for (int i = 1; i < n; i++) {
-        int a, w;
-        cin >> a >> w;
-        adj[i+1].push_back({a, w});
-        adj[a].push_back({i+1, w});
+Node *findLCA(Node *root, int n1, int n2)
+{
+    if (root == NULL)
+        return NULL;
+
+    if (root->data > n1 && root->data > n2)
+        return findLCA(root->left, n1, n2);
+
+    if (root->data < n1 && root->data < n2)
+        return findLCA(root->right, n1, n2);
+
+    return root;
+}
+
+int findDistance(Node *root, int k, int distance)
+{
+    if (root == NULL)
+        return -1;
+
+    if (root->data == k)
+        return distance;
+
+    int left = -1;
+    if (root->left != NULL && root->data > k)
+    {
+        left = findDistance(root->left, k, distance + root->left->jarak);
     }
-    cin >> t;
-    while (t--) {
-        int b;
-        cin >> b;
-        bfs(b);
-        int ans = 1e9;
-        for (int i = 1; i <= n; i++) {
-            if (parent[i] != -1) {
-                ans = min(ans, dist[i] + dist[b] - 2 * dist[parent[i]]);
+
+    if (left != -1)
+        return left;
+
+    int right = -1;
+    if (root->right != NULL && root-> data < k)
+    {
+        right = findDistance(root->right, k, distance + root->right->jarak);
+    }
+
+    return right;
+}
+
+int findShortestPath(Node *root, int n1, int n2)
+{
+    Node *lca = findLCA(root, n1, n2);
+    int d1 = findDistance(lca, n1, 0);
+    int d2 = findDistance(lca, n2, 0);
+    return d1 + d2;
+}
+
+void findLeaf(Node *root, vector<int> &leaf)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    if (root->left == NULL && root->right == NULL)
+    {
+        leaf.push_back(root->data);
+    }
+    else
+    {
+        findLeaf(root->left, leaf);
+        findLeaf(root->right, leaf);
+    }
+}
+
+int main()
+{
+    vector<int> leaf;
+    init(&pohon);
+    int n, data, jarak;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            cin >> data;
+            InsertTree(&pohon, data, 0);
+        }
+        else
+        {
+            cin >> data >> jarak;
+            InsertTree(&pohon, data, jarak);
+        }
+    }
+    findLeaf(pohon.root, leaf);
+    int query;
+    cin >> query;
+    Node *temp;
+    int shortestPath = 0;
+    for (int i = 0; i < query; i++)
+    {
+        int angka;
+        cin >> angka;
+        temp = search(pohon.root, angka);
+        int nilai_minimal = 1e9;
+        for (int j = 0; j < leaf.size(); j++)
+        {
+            shortestPath = findShortestPath(pohon.root, leaf[j], temp->data);
+            if (shortestPath < nilai_minimal)
+            {
+                nilai_minimal = shortestPath;
             }
         }
-        cout << ans << " ";
+        cout << nilai_minimal;
+        if (i != query - 1)
+        {
+            cout << " ";
+        }
     }
     cout << endl;
     return 0;
